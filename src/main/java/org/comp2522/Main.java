870598ccdc68e43bb0abfa9d336072df43341593
Aryan Rocks <aryan.jand@gmail.com>
2022-09-06 11:04:18 -0700

    Hey Paul,
    part 2

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * Hi Paul.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi COMP2522!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hi COMP2522!");
+  }
 }
